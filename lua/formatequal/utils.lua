18e7d049c848d29f5a65e6dbeec65d09f04b5d25
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2022-09-13 17:04:47 +0100

    feat(utils): added new function to wrap around filter

--- lua/formatequal/utils.lua
+++ lua/formatequal/utils.lua
@@ -7,6 +7,16 @@ local function insert_string(str1, str2, pos)
     return str1:sub(1,pos) .. str2 .. str1:sub(pos+1)
 end
 
+---Check if a line should be ignored according to a filter
+---@param line string #String to be parsed
+---@param filter function #Function to check the condition
+local function ignore_this_line(line, filter)
+    if (filter == nil) then return false end
+    return filter(line)
+end
+
+
 return {
     insert_string = insert_string,
+    ignore_this_line = ignore_this_line,
 }
