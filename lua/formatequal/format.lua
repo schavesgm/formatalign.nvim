3d7733bdb0f4d7796d36b4f4ef8c6ad3a1ffef29
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2022-09-13 17:14:07 +0100

    feat(format): added a collection of functions to perform equal formatting

--- lua/formatequal/format.lua
+++ lua/formatequal/format.lua
@@ -1,4 +1,5 @@
-local utils = require"formatequal.utils"
+local ignore_this_line = require"formatequal.utils".ignore_this_line
+local insert_string    = require"formatequal.utils".insert_string
 
 ---Get all spaces between position in string and first non-space word
 ---Example:
@@ -33,18 +34,71 @@ end
 ---Format all lines according to format_line_to_sign
 ---@param lines table #Table containing all lines to be formatted
 ---@param sign string #Character used as special sign
----@param ignore_line_filter function #Callback funtion that sets if a line should be ignored
+---@param ignore_line_filter function #Callback funtion that checks if a line should be ignored
 local function format_lines(lines, sign, ignore_line_filter)
     local new_lines = {}
     for _, line in ipairs(lines) do
-        local new_line = utils.ignore_this_line(line, ignore_line_filter) and line or format_line_to_sign(line, sign)
+        local new_line = ignore_this_line(line, ignore_line_filter) and line or format_line_to_sign(line, sign)
         table.insert(new_lines, new_line)
     end
     return new_lines
+end
+
+---Get the longest column at which sign is located in a collection of lines
+---The lines should be previously formatted according to format_lines
+---@param lines table #Table containing all lines to be parsed
+---@param sign string #Character used as special sign
+---@param ignore_line_filter function #Callback funtion that checks if a line should be ignored
+local function get_longest_sign_column(lines, sign, ignore_line_filter)
+    local longest_column = -1
+    for _, line in ipairs(lines) do
+        if ignore_this_line(line, ignore_line_filter) then
+            goto continue
+        end
+        longest_column = math.max(longest_column, string.find(line, sign) or -1)
+        ::continue::
+    end
+    return longest_column
+end
+
+---Format all files to a particular position; the lines should be previously formatted
+---according to format_lines
+---@param lines table #Table containing all lines to be parsed
+---@param sign string #Character used as special sign
+---@param longest_col number #Maximum length where the sign should be present
+---@param ignore_line_filter function #Callback funtion that checks if a line should be ignored
+local function format_to_longest_col(lines, sign, longest_col, ignore_line_filter)
+    local new_lines = {}
+    for _, line in ipairs(lines) do
+        local current_sign = string.find(line, sign)
+        if ignore_this_line(line, ignore_line_filter) or (current_sign == nil) then
+            table.insert(new_lines, line)
+        else
+            local new_line = insert_string(line, string.rep(" ", longest_col - current_sign), current_sign - 1)
+            table.insert(new_lines, new_line)
+        end
+    end
+    return new_lines
+end
+
+
+---Format a given selection of lines
+local function format_selected_lines(sign, ignore_line_filter)
+    local bufnr = vim.api.nvim_get_current_buf()
+    local slnr  = vim.fn.line("'<")
+    local elnr  = vim.fn.line(">'")
+    local lines = vim.fn.getline(slnr, elnr)
+    lines = format_lines(lines, sign, ignore_line_filter)
+    local longest_col = get_longest_sign_column(lines, sign, ignore_line_filter)
+    lines = format_to_longest_col(lines, sign, longest_col, ignore_line_filter)
+    vim.api.nvim_buf_set_lines(bufnr, slnr - 1, elnr, true, lines)
 end
 
 return {
     format_line_to_sign = format_line_to_sign,
     get_spaces_before_position = get_spaces_before_position,
     format_lines = format_lines,
+    get_longest_sign_column = get_longest_sign_column,
+    format_to_longest_col = format_to_longest_col,
+    format_selected_lines = format_selected_lines,
 }
