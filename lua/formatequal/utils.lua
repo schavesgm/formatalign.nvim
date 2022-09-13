f26c4a685df136b69d779bd4f5cbd78aa1e07207
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2022-09-13 16:54:41 +0100

    fix(utils): corrected bound checking bug in insert_string function

--- lua/formatequal/utils.lua
+++ lua/formatequal/utils.lua
@@ -1,9 +1,9 @@
----Insert a string inside another substring at a given position
+---Insert a string inside another substring at a given position, performs bound checking
 ---@param str1 string #Base string
 ---@param str2 string #Substring to be inserted into str1
 ---@param pos number #Position where str2 is inserted
 local function insert_string(str1, str2, pos)
-    assert(1 <= pos and pos <= #str1)
+    assert(0 <= pos and pos <= #str1)
     return str1:sub(1,pos) .. str2 .. str1:sub(pos+1)
 end
 
