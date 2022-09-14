8fd5a347db84fbf4d0b48e4d1bfb727af12e2585
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2022-09-14 13:20:22 +0100

    fix(plugin): the keybinding is now a expression instead of a callback function to avoid visual mark bug

--- lua/formatequal.lua
+++ lua/formatequal.lua
@@ -8,32 +8,26 @@ local ignore_functions  = require"formatequal.ignore"
 -- Value that sets that a filetype should be ignored
 M.IGNORE = 0
 
--- Default settings of the module
-M.settings = {
-    Python   = {signs={'=', ':'}, ignore_function=ignore_functions.python},
-    Lua      = {signs={'='}, ignore_function=ignore_functions.lua},
-    Tex      = M.IGNORE,
-    Markdown = M.IGNORE,
-}
-
 --Format a selection of lines hierarchically, according to settings.signs
 function M.format_hierarchically()
     local filetype = vim.bo.filetype:gsub('^%l', string.upper)
-    local settings = M.settings[filetype]
+    local settings = M.settings.filetype[filetype]
 
     -- Ignore this call if necessary
-    if settings == M.IGNORE then
+    if (settings == false) then
         vim.notify(string.format('formatequal is ignored in %s files', filetype), vim.log.levels.WARN)
         return
     end
 
-    local ordered_signs = {'='}
-    if settings then ordered_signs = settings.signs end
-
-    local ignore_filter = (settings) and settings.ignore_function or nil
+    local ordered_signs, ignore_filter = {"="}, nil
+    if settings then
+        ordered_signs = settings.signs or ordered_signs
+        ignore_filter = settings.ignore_function or nil
+    end
 
     local slnr, elnr   = vim.fn.line("'<"), vim.fn.line("'>")
     local bufnr, lines = vim.api.nvim_get_current_buf(), vim.fn.getline(slnr, elnr)
+
     for _, sign in pairs(ordered_signs) do
          if any_contains_sign(lines, sign, ignore_filter) then
              lines = formatequal_lines(lines, sign, ignore_filter)
@@ -46,8 +40,24 @@ end
 ---Setup function used to set some hierarchical special characters
 ---@param settings table #Settings table to update the plugin
 function M.setup(settings)
-    if not settings then return end
-    M.settings = vim.tbl_deep_extend('force', M.settings, settings)
+    -- Default settings of the module
+    M.settings = {
+        keybinding = {
+            lhs = '<leader>=',
+            set = true,
+        },
+        filetype = {
+            Python   = {signs={'=', ':'}, ignore_function=ignore_functions.python},
+            Lua      = {signs={'='}, ignore_function=ignore_functions.lua},
+            Tex      = false,
+            Markdown = false,
+        },
+    }
+    M.settings = (settings) and vim.tbl_deep_extend('force', M.settings, settings) or M.settings
+    local keybinding = M.settings.keybinding
+    if keybinding.set then
+        vim.keymap.set('v', keybinding.lhs, ':lua require"formatequal".format_hierarchically()<Cr>', {silent=true})
+    end
 end
 
 return M
